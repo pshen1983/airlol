a8c5eae729a19dca5b779f269680b5af355d8777
Peng Shen <peng_shen@Peng-S-MBP13.local>
2016-03-03 16:59:25 -0800

    implementation

--- page/view/template/footer.php
+++ page/view/template/footer.php
@@ -1,6 +1,7 @@
 <div id="footer">
 </div>
-<script src="/page/js/jquery-2.2.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="/page/js/common.js"></script>
 <?php foreach (View::$javascripts as $js) { ?>
 <script src="<?=$js ?>"></script>
