399c40c1fc4477d11cd04c58813174dcbea906c0
Peng Shen <peng_shen@Peng-S-MBP13.local>
2016-06-22 15:16:02 -0700

    add copyright to footer

--- view/template/footer.php
+++ view/template/footer.php
@@ -1,4 +1,11 @@
 <footer class="container-fluid">
+<div id="footer" >
+<div id="footer-mail">
+</div>
+<div id="footer-bottom">
+&copy; <?=date('Y')?>, AirLOL Inc. All rights reserved.
+</div>
+</div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
