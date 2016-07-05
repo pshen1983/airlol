9f690e785e1e63834d4ba94c628ebe6f06bf0261
Peng Shen <peng_shen@Peng-S-MBP13.local>
2016-07-05 14:12:04 -0700

    add dropdown menu

--- view/template/header.php
+++ view/template/header.php
@@ -20,12 +20,18 @@
 </div>
 <div id="right">
 <?php if (View::$params['user_session']) { ?>
-    <a href="/history"><?=View::$content['btn_header_history'] ?></a>
-    <a href="/message/list"><?=View::$content['btn_header_message'] ?></a>
-    <a id="message"><?=View::$params['header_user_name'] ?><img id="pic" src="<?=View::$params['header_user_pic'] ?>"/></a>
+    <a id="history_a" class="head" href="/history"><?=View::$content['btn_header_history'] ?></a>
+    <a id="message_a" class="head" href="/message/list"><?=View::$content['btn_header_message'] ?></a>
+    <a id="profile_a" class="head"><?=View::$params['header_user_name'] ?><img id="pic" src="<?=View::$params['header_user_pic'] ?>"/></a>
+    <div id="profile_div" class="dropdown">
+    <a class="menu" href="/profile">Edit Profile</a>
+    <a class="menu" href="">Account Settings</a>
+    <a class="menu" href="">My Guidebook</a>
+    <a class="menu" href="/logout">Sign Out</a>
+    </div>
 <?php } else { ?>
-    <a id="singin" type="button" class="hbtn" data-toggle="modal" data-target="#header_login"><?=View::$content['btn_header_signin'] ?></a>
-    <a id="singup" type="button" class="hbtn" data-toggle="modal" data-target="#header_login"><?=View::$content['btn_header_signup'] ?></a>
+    <a id="singin" class="head" type="button" class="hbtn" data-toggle="modal" data-target="#header_login"><?=View::$content['btn_header_signin'] ?></a>
+    <a id="singup" class="head" type="button" class="hbtn" data-toggle="modal" data-target="#header_login"><?=View::$content['btn_header_signup'] ?></a>
 <?php } ?>
 </div>
 </header>
