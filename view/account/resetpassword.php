dc56f269afe3ae57692410d185312a89ffe15a01
pshen1983 <pshen1983@gmail.com>
2016-07-16 00:00:08 -0700

    style reset password page

--- view/account/resetpassword.php
+++ view/account/resetpassword.php
@@ -1,8 +1,13 @@
-<?=$params['status'] ?> Reset Password:
+<?=$params['status'] ?>
+<div id="outer">
+<div id="sign">
+<h4>Reset Password</h4>
 <form id="forgetpasswd" method="post" action="/reset-password">
-New Password: <input id="email" type="password" name="passwd0" />
-Re-Enter Passwd: <input id="email" type="password" name="passwd1" />
+<input class="inputbox" id="email" type="password" name="passwd0" placeholder="New Password" />
+<input class="inputbox" id="email" type="password" name="passwd1" placeholder="Re-Enter Passwd" />
+<input type="submit" class="round_6 long_btn"  value="<?=$params['submit'] ?>" />
 <input type="hidden" name="p" value="<?=$params['p'] ?>" />
 <input type="hidden" name="token" value="<?=$params['token'] ?>" />
-<input type="submit" value="<?=$params['submit'] ?>" />
 </form>
+</div>
+</div>
