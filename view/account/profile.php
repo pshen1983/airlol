3d03e04e82dab85a6d01dfed7919d6b6e7ebcfc4
pshen1983 <pshen1983@gmail.com>
2016-07-23 00:17:12 -0700

    style profile page

--- view/account/profile.php
+++ view/account/profile.php
@@ -4,23 +4,24 @@
 </div>
 <form id="signin" method="post" action="/profile">
 <div class="table">
+<div class="table-title"><h4>Edit Profile</h4></div>
 <div class="row">
-<div class="cell text-right">email</div><div class="cell">email</div>
+<div class="cell text-right">Email</div><div class="cell">email</div>
 </div>
 <div class="row">
-<div class="cell text-right">name</div><div class="cell"><input name="user[name]" value="" /></div>
+<div class="cell text-right">Name</div><div class="cell"><input name="user[name]" value="" /></div>
 </div>
 <div class="row">
-<div class="cell text-right">password</div><div class="cell"><a href="" >reset password email</a></div>
+<div class="cell text-right">Password</div><div class="cell"><a href="" >reset password email</a></div>
 </div>
 <div class="row">
-<div class="cell text-right">birth day</div><div class="cell"><select name="user[month]"></select> <input name="user[year]" value="" /></div>
+<div class="cell text-right">Birth Date</div><div class="cell"><select name="user[month]"></select> <input style="width:60px;padding:10px;" name="user[year]" value="" /></div>
 </div>
 <div class="row">
-<div class="cell text-right">phone</div><div class="cell"><input name="user[tel]" value="" /></div>
+<div class="cell text-right">Phone</div><div class="cell"><input name="user[tel]" value="" /></div>
 </div>
 <div class="row">
-<div class="cell text-right">wechat</div><div class="cell"><input name="user[wechat]" value="" /></div>
+<div class="cell text-right">Wechat</div><div class="cell"><input name="user[wechat]" value="" /></div>
 </div>
 <div class="row">
 <div class="cell text-right">Preferred Language</div><div class="cell"><select name="user[language]"></select></div>
