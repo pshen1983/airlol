45c2eda0e3f3c2632b23f1b46a03cee2830c611c
Shen <shenpeng@c4b301c27f23.ant.amazon.com>
2017-04-15 11:49:42 -0700

    refactoring for frontend

---
+++ page/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Fuck the World</title>
+<link rel="shortcut icon" href="/page/img/favicon.png">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/page/css/common.css">
+<link rel='stylesheet' type='text/css' href='/page/css/google-poppins.css'>
+</head>
+<body>
+index
+</body>
+</html>
