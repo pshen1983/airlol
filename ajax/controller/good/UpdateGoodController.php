211c4b7323eea7446a2a5f1a31abdd9c3dfb78b4
Shen <shenpeng@c4b301c27f23.ant.amazon.com>
2017-06-01 22:08:54 -0700

    add doc for update trip/good apis

--- ajax/controller/good/UpdateGoodController.php
+++ ajax/controller/good/UpdateGoodController.php
@@ -6,16 +6,16 @@ class UpdateGoodController extends AjaxController {
 
         $goodDao = $params['good'];
 
-        if (isset($params['departure'])) { $goodDao->setDepartureCode($params['departure']); }
-        if (isset($params['arrival'])) { $goodDao->setArrivalCode($params['arrival']); }
+        if (isset($params['departure_code'])) { $goodDao->setDepartureCode($params['departure_code']); }
+        if (isset($params['arrival_code'])) { $goodDao->setArrivalCode($params['arrival_code']); }
         if (isset($params['end_date'])) { $goodDao->setEndDate($params['end_date']); }
         if (isset($params['type'])) { $goodDao->setGoodType($params['type']); }
-        if (isset($params['unit'])) { $goodDao->setGoodUnit($params['unit']); }
-        if (isset($params['description'])) { $goodDao->setDescription($params['description']); }
-        if (isset($params['active'])) { $goodDao->setActive($params['active']); }
-        if (isset($params['contact'])) { $goodDao->setContactType($params['contact']); }
+        if (isset($params['weight'])) { $goodDao->setGoodUnit($params['weight']); }
+        if (isset($params['weight_unit'])) { $goodDao->setDescription($params['weight_unit']); }
+        if (isset($params['description'])) { $goodDao->setActive($params['description']); }
         if (isset($params['price'])) { $goodDao->setPrice($params['price']); }
-        if (isset($params['price_adjust'])) { $goodDao->setPriceAdjust($params['price_adjust']); }
+        if (isset($params['currency'])) { $goodDao->setContactType($params['currency']); }
+        if (isset($params['active'])) { $goodDao->setPriceAdjust($params['active']); }
 
         if (!$goodDao->save()) {
             $this->setStatusCode(500);
--- ajax/controller/trip/UpdateTripController.php
+++ ajax/controller/trip/UpdateTripController.php
@@ -6,16 +6,15 @@ class UpdateTripController extends AjaxController {
 
         $tripDao = $params['trip'];
 
-        if (isset($params['departure'])) { $tripDao->setDepartureCode($params['departure']); }
-        if (isset($params['arrival'])) { $tripDao->setArrivalCode($params['arrival']); }
+        if (isset($params['departure_code'])) { $tripDao->setDepartureCode($params['departure_code']); }
+        if (isset($params['arrival_code'])) { $tripDao->setArrivalCode($params['arrival_code']); }
         if (isset($params['trip_date'])) { $tripDao->setTripDate($params['trip_date']); }
         if (isset($params['space_type'])) { $tripDao->setSpaceType($params['space_type']); }
-        if (isset($params['space_num'])) { $tripDao->setSpaceNum($params['space_num']); }
-        if (isset($params['space_unit'])) { $tripDao->setSpaceUnit($params['space_unit']); }
+        if (isset($params['weight'])) { $tripDao->setSpaceNum($params['weight']); }
+        if (isset($params['weight_unit'])) { $tripDao->setSpaceUnit($params['weight_unit']); }
         if (isset($params['active'])) { $tripDao->setActive($params['active']); }
-        if (isset($params['contact'])) { $tripDao->setContactType($params['contact']); }
+        if (isset($params['currency'])) { $tripDao->setContactType($params['currency']); }
         if (isset($params['price'])) { $tripDao->setPrice($params['price']); }
-        if (isset($params['price_adjust'])) { $tripDao->setPriceAdjust($params['price_adjust']); }
 
         if (!$tripDao->save()) {
             $this->setStatusCode(500);
