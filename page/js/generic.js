8bc00a49315b93a2057c6f13e5dbb0ec4e6894f5
Custa <custa@custa.local>
2018-03-14 16:59:55 -0700

    implement home page

--- page/js/generic.js
+++ page/js/generic.js
@@ -1,29 +1,30 @@
-$("#trip_next").on("click", function() {
-    $.post(
-        "ajax/trip", 
-        $($(this)[0].form).serialize(),
-        function (data) {
-            obj = JSON.parse(data);
-
-            alert(obj.message);
-            if (obj.status==0) {
-
-            }
-        }
-    );
+$(function() {
+    $("#package_btn").on("click", function(){
+        document.getElementById('travelling_div').style.display='none';
+        document.getElementById('package_div').style.display='block';
+        document.getElementById('travelling_btn').style.backgroundColor="";
+        document.getElementById('package_btn').style.backgroundColor="#ffca59";
+        document.getElementById('oval_top').style.backgroundColor="#ffca59";
+    });
+    $("#travelling_btn").on("click", function(){
+        document.getElementById('travelling_div').style.display='block';
+        document.getElementById('package_div').style.display='none';
+        document.getElementById('package_btn').style.backgroundColor="";
+        document.getElementById('travelling_btn').style.backgroundColor="#11859e";
+        document.getElementById('oval_top').style.backgroundColor="#11859e";
+    });
 });
 
-$("#good_next").on("click", function() {
-    $.post(
-        "ajax/good", 
-        $($(this)[0].form).serialize(),
-        function (data) {
-            obj = JSON.parse(data);
-
-            alert(obj.message);
-            if (obj.status==0) {
-
-            }
-        }
-    );
-});
+function ovalSelect(evt, elementName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("oval_content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("oval_option");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" oval_select", "");
+    }
+    document.getElementById(elementName).style.display = "block";
+    evt.currentTarget.className += " oval_select";
+}
--- view/generic/index.php
+++ view/generic/index.php
@@ -1,4 +1,21 @@
-Home Page
-<?php View::addTemplate('trip_search', $params['airports']); ?>
-<br>
-<?php View::addTemplate('good_search', $params['airports']); ?>
+<div style="width:90%;margin: 0 auto;">
+	<div class="oval_btns">
+		<button id="package_btn" class="oval_option" style="background-color: #ffca59;"><label class="oval_text">I have a package</label></button>
+		<button id="travelling_btn" class="oval_option"><label class="oval_text">I am travelling</label></button>
+	</div>
+	<div class="oval_selects">
+		<div id="oval_top"><label></label></div>
+		<div id="package_div" class="oval_content" style="display:block">
+		Package
+		<?php View::addTemplate('trip_search', $params['airports']); ?>
+		<br>
+		<?php View::addTemplate('good_search', $params['airports']); ?>
+		</div>
+		<div id="travelling_div" class="oval_content">
+		Travelling
+		<?php View::addTemplate('trip_search', $params['airports']); ?>
+		<br>
+		<?php View::addTemplate('good_search', $params['airports']); ?>
+		</div>
+	</div>
+</div>
